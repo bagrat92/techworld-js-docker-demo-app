6f08aaf04a129f0d55a270ecdd41f6d570ebcebc
Bagrat Harutyunyan <bagrat@superannotate.com>
2025-06-09 00:25:55 +0400

    more changes

--- app/server.js
+++ app/server.js
@@ -21,10 +21,12 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = "mongodb://admin:Pa$$w08d@localhost:27017";
 
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+let mongoUrlDocker = "mongodb://admin:Pa$$w08d@mongodb";
+
+let mongoUrlKubernetes = "mongodb-service.default.svc.cluster.local:27017";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -35,7 +37,7 @@ let databaseName = "user-account";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlKubernetes, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -57,7 +59,7 @@ app.post('/update-profile', function (req, res) {
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlKubernetes, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
